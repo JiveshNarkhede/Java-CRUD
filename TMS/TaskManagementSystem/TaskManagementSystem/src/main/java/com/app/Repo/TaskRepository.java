3aa7519f656ef1d3154f2959a6f941cbe9871c20
JiveshNarkhede <narkhedejivesh@gmail.com>
2024-01-08 14:17:12 +0530

    overall Assignment

---
+++ TMS/TaskManagementSystem/TaskManagementSystem/src/main/java/com/app/Repo/TaskRepository.java
@@ -0,0 +1,11 @@
+package com.app.Repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.app.entity.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Integer> {
+
+}
